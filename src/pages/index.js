72a1efc28a36626a83b2e145f7875d64a9d976b3
Aleksey Samusenko <alex.samusenko@gmail.com>
2021-07-08 01:15:07 +0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Ваня отпускает рыбу"
+        src="../images/20150803_080601000_iOS.jpg"
+      />
     </Layout>
   )
 }
